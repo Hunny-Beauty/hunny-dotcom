0f688b2cc5af2fc7e6b411e1b2ec348319c7386e
Camrynn Dilley <dorkycam@gmail.com>
2018-09-15 19:52:26 -0700

    added routing and added all the page components

---
+++ hunny-app/src/app/components/bios/bios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bios',
+  templateUrl: './bios.component.html',
+    styleUrls: ['./bios.component.css']
+})
+export class BiosComponent {
+  title = 'bios';
+}
