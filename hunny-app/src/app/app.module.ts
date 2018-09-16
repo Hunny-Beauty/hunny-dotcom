dc385375f22da3b19dec6f431313ea84ce8136c4
Camrynn Dilley <dorkycam@gmail.com>
2018-09-15 19:51:53 -0700

    added routing - not styled yet

--- hunny-app/src/app/app.module.ts
+++ hunny-app/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { BiosComponent } from './components/bios/bios.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { SocialComponent } from './components/social/social.component';
+import { StyleGuideComponent } from './components/style-guide/style-guide.component';
+import { TutorialsComponent } from './components/tutorials/tutorials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    BiosComponent,
+    ContactComponent,
+    HomeComponent,
+    SocialComponent,
+    StyleGuideComponent,
+    TutorialsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'header', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'bios', component: BiosComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'social', component: SocialComponent },
+      { path: 'style-guide', component: StyleGuideComponent },
+      { path: 'tutorials', component: TutorialsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
